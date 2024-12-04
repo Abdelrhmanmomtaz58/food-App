40a9071ad33c8f1a48a940046fe57382c9c0e58f
Abdelrhman Momtaz <140316365+Abdelrhmanmomtaz58@users.noreply.github.com>
2024-12-04 05:57:22 +0200

    xml home fragment

---
+++ app/src/main/java/com/momtaz/food/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.momtaz.food
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
+import com.momtaz.food.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        // Change the fragment in the activity according to the icon in the navigation button
+        val navigationController = Navigation.findNavController(this,R.id.host_fragment)
+        NavigationUI.setupWithNavController(binding.btmNav,navigationController)
+        // end
+    }
+}
